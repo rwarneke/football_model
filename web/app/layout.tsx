82c13a2afc02ce08e73dc74f5ec255bbed4a4679
Rowan Warneke <rowanwarneke@gmail.com>
2026-02-07 09:56:54 +1100

    favicon

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -24,6 +24,7 @@ export const metadata: Metadata = {
     icon: [
       { url: "/favicon.ico" },
       { url: "/favicon.svg", type: "image/svg+xml" },
+      { url: "/favicon-48.png", sizes: "48x48", type: "image/png" },
       { url: "/favicon-32.png", sizes: "32x32", type: "image/png" },
       { url: "/favicon-16.png", sizes: "16x16", type: "image/png" },
     ],
